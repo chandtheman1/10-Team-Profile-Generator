f8e259894f56a8b593429ba40ed1088684cd2e9a
Chandler Liang <chandler.liang123@gmail.com>
2021-08-04 18:11:03 +1000

    updated intern.test.js

--- _tests_/intern.test.js
+++ _tests_/intern.test.js
@@ -7,6 +7,25 @@ describe("Intern", () => {
     });
 
     describe("Constructor", () => {
+
+        it("can set the name", () => {
+            const testValue = "Chandler";
+            const test = new Intern(testValue);
+            expect(test.name).toBe(testValue);
+        });
+
+        it("can set the ID", () => {
+            const testValue = 1;
+            const test = new Intern("Chandler", testValue);
+            expect(test.id).toBe(testValue);
+        });
+
+        it("can set email", () => {
+            const testValue = "welcome@hello.com";
+            const test = new Intern("Chandler", 1, testValue);
+            expect(test.email).toBe(testValue);
+        });
+
         it("can set school", () => {
             const testValue = "Monash"
             const test = new Intern("Chandler", 1, "welcome@hello.com", testValue);
